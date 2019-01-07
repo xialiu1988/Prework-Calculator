0533be22635533fdbe7413148b8bb1c8219703ea
xialiu1988 <liuxia198896@gmail.com>
2019-01-07 12:11:56 -0800

    'updated

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Prework_Calculator
             double a=double.Parse(Console.ReadLine());
             Console.WriteLine("Please enter the second number:");
             double b=double.Parse(Console.ReadLine());
-
+            //choose what operation you want
            Console.WriteLine("What you want to do? Add:press 1; Substract: press 2; Multiply: press 3; Divide: Press 4.");
             int c=Int32.Parse(Console.ReadLine());
             if(c==1){Add(a,b);}
@@ -19,7 +19,7 @@ namespace Prework_Calculator
             else if(c==3){Multiply(a,b);}        
             else{Divide(a,b);}
         }
-    
+    //sum
         public static void Add(double a, double b){
   
         Console.WriteLine($"The sum is:{a+b}");
@@ -27,20 +27,21 @@ namespace Prework_Calculator
 
          }
 
-
+        //substraction
          public static void Subtract(double a, double b){
 
         Console.WriteLine($"The result of subtraction is:{a-b}");
        
 
          }
-
+        //production
          public static void Multiply(double a, double b){
  
         Console.WriteLine($"The production is:{a*b}");
          
          }
 
+        //division
         public static void Divide(double a,double b){
         double result=0.0;
          
